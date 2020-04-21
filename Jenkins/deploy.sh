50d4d92c9e46a9916594e4b444a0670059d02476
Manas-kashyap <manaskashyaptech@gmail.com>
2020-04-21 11:28:23 +0530

    modofied Jenkins deploy file

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 # scp -r ./target/scala-2.11/*.jar 35.202.16.214:~/artifact
 # ssh 35.202.16.214
 # sudo apt install default-jdk -y
-java -jar ~/artifact/*.jar
+
+java -jar akka-http-helloworld-assembly-1.0.jar
