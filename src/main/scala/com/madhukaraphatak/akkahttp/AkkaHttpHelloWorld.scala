1d64f6154840c7eb9d74ff410eb93feda4435af5
Manas-kashyap <manaskashyaptech@gmail.com>
2020-04-21 11:20:39 +0530

    modified Jenkisn deploy file and package for deploy

--- src/main/scala/com/madhukaraphatak/akkahttp/AkkaHttpHelloWorld.scala
+++ src/main/scala/com/madhukaraphatak/akkahttp/AkkaHttpHelloWorld.scala
@@ -25,7 +25,7 @@ object AkkaHttpHelloWorld {
           }
         }
       }
-    Http().bindAndHandle(route,"localhost",8080)
+    Http().bindAndHandle(route,"localhost",3000)
 
     println("server started at 8080")
   }
